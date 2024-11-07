3c76ab3f6858e0491a9bfcbdd775d2aaf30dd9b1
Vane <20233tn112@utez.edu.mx>
2024-11-07 11:18:22 -0600

    Subo cambios en el diseño de las cards

--- PrototipTwo/js/script.js
+++ PrototipTwo/js/script.js
@@ -21,38 +21,6 @@ function filterCards(status) {
     });
 }
 
-// Selección y resaltado de cards
-const cards = document.querySelectorAll('.card');
-let selectedCard = null;
-let clickCount = 0;
-
-cards.forEach(card => {
-    card.addEventListener('click', (event) => {
-        event.stopPropagation();
-
-        // Verificar si se hizo clic en la misma tarjeta
-        if (selectedCard === card) {
-            clickCount++;
-        } else {
-            // Si se hace clic en una tarjeta diferente, reiniciar el contador
-            clickCount = 1;
-            selectedCard = card;
-
-            // Remover la clase 'highlight' de todas las tarjetas
-            removeHighlightFromAllCards();
-
-            // Agregar la clase 'highlight' a la tarjeta actual
-            card.classList.add('highlight');
-        }
-
-        // Abrir el modal de edición en el segundo clic
-        if (clickCount === 2) {
-            openEditModal();
-            clickCount = 0; 
-        }
-    });
-});
-
 // Función para abrir el modal de registro
 function openModal() {
     const modal = document.getElementById("registerModal");
@@ -80,15 +48,9 @@ function closeModal(modalId) {
     }, 300);
 }
 
-// Remover el brillo de todas las tarjetas
-function removeHighlightFromAllCards() {
-    cards.forEach(c => c.classList.remove('highlight'));
-}
-
 // Cerrar el modal al hacer clic fuera del contenido
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
         closeModal(event.target.id);
     }
-    removeHighlightFromAllCards(); // Quitar el brillo de las cards al hacer clic fuera de las cards
 }
