cbec086866f078dbdd09cadd2c8a8eecfb5a1b74
Apaez200 <20233tn139>
2024-11-12 21:03:32 -0600

    menu de hamburguesa

--- PrototipTwo/js/script.js
+++ PrototipTwo/js/script.js
@@ -336,4 +336,26 @@ function filterCards(status) {
     });
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const sideMenu = document.querySelector('.side-menu');
+
+    function toggleMenu() {
+        sideMenu.classList.toggle('active');
+    }
+
+    menuToggle.addEventListener('click', toggleMenu);
+
+    
+    const closeBtn = document.querySelector('.close-btn');
+    if (closeBtn) {
+        closeBtn.addEventListener('click', toggleMenu);
+    }
+});
+
+
+
+
+
+
 
